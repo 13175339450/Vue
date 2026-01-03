e32cae6035f2591cd249e23e0ac261565a1b3f2c
hxl <hxl@git.com>
2026-01-03 10:36:29 +0800

    [2026/1/3]pinia准备

---
+++ Vue-Pinia/src/main.ts
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// 1.引入pinia
+import {createPinia} from 'pinia'
+
+const app = createApp(App);
+
+// 2.创建pinia
+const pinia = createPinia();
+
+// 3.安装pinia
+app.use(pinia)
+
+app.mount('#app')
