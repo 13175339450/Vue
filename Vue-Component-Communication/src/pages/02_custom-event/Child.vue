a22b74402cd59a560518c894b0841c246ee71885
hxl <hxl@git.com>
2026-01-04 22:46:32 +0800

    [2026/1/4]自定义事件进行子传父

---
+++ Vue-Component-Communication/src/pages/02_custom-event/Child.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="child">
+        <h3>子组件</h3>
+        <hr>
+        <button @click="emit('send-info', sonInfo)">给父亲发送信息</button>
+    </div>
+</template>
+
+<script lang="ts" setup name="child-props">
+    import { ref } from 'vue'; 
+
+    let sonInfo = ('SON')
+    // 声明事件
+    const emit = defineEmits(['send-info'])
+</script>
+
+<style scoped>
+    .child {
+        background-color: skyblue;
+        padding: 10px;
+        box-shadow: 0 0 10px black;
+        border-radius: 10px;
+    }
+</style>
--- Vue-Component-Communication/src/pages/02_custom-event/Father.vue
+++ Vue-Component-Communication/src/pages/02_custom-event/Father.vue
@@ -1,11 +1,35 @@
 <template>
-    
+    <div class="father">
+        <h3>父组件</h3>
+        <hr>
+        儿子传来的: {{ sonInfo }}
+        <Child @send-info="getInfo"/>
+    </div>
 </template>
 
-<script lang="ts" setup name="father-event">
+<script lang="ts" setup name="father-props">
+    import { ref } from 'vue';
+    import Child from './Child.vue';
+
+    const sonInfo = ref('')
+
+    function getInfo(value:string) {
+        sonInfo.value = value;
+    }
 
 </script>
 
 <style scoped>
+    .father{
+        /* 居中 */
+        /* 1. 设置一个宽度 */
+        width: 900px;
+        /* 2. 设置左右外边距为 auto，浏览器会自动平分剩余空间 */
+        margin: 0 auto;
 
+        
+        background-color: rgb(165, 164, 164);
+        padding: 20px;
+        border-radius: 10px;
+    }
 </style>
