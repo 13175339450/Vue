158696a4dfea2d5b5f4434f6db2434caa6f6352a
hengxiaoliang <hengxl@vvmeeting.com>
2026-01-05 17:43:18 +0800

    [2026/1/5]默认插槽slot

---
+++ Vue-Component-Communication/src/pages/09_slot/Category.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="category">
+        <h2>{{ title }}</h2>
+        <!-- 插槽位置 -->
+        <slot> <h1>没有则展示默认值</h1> </slot>
+    </div>
+</template>
+
+<script setup>
+    defineProps(['title'])
+</script>
+
+<style scoped>
+    .category{
+        background-color: skyblue;
+        border-radius: 10px;
+        box-shadow: 0 0 10px;
+        padding: 10px;
+        width: 200px;
+        height: 300px;
+    }
+
+    h2 {
+        /* 文字居中 */
+        text-align: center;
+        background-color: rgb(42, 165, 89);
+        border-radius: 10px;
+    }
+</style>
